c52389aaf580f93506e1710927047d7aa745e22f
Munem <munem.soufi@proton.me>
2024-12-15 22:08:10 +0100

    add search function

--- blog/accountnav.php
+++ blog/accountnav.php
@@ -2,9 +2,9 @@
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
-    <!-- <img src="https://icons.iconarchive.com/icons/emey87/social-button/16/blog-icon.png" width="16" height="16" alt="Blog Icon"> -->
-            <img src="https://icons.iconarchive.com/icons/emey87/social-button/32/blog-icon.png" 
-                 width="32" height="32" alt="Blog Icon"> Blog
+            <!-- <img src="https://icons.iconarchive.com/icons/emey87/social-button/16/blog-icon.png" width="16" height="16" alt="Blog Icon"> -->
+            <img src="https://icons.iconarchive.com/icons/emey87/social-button/32/blog-icon.png" width="32" height="32"
+                alt="Blog Icon"> Blog
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor04"
             aria-controls="navbarColor04" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,21 +18,21 @@
                         <span class="visually-hidden">(current)</span>
                     </a>
                 </li>
-                <?php 
-                if (isset($_COOKIE['username'])) {
-                ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
-                </li>
                 <?php
+                if (isset($_COOKIE['username'])) {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                    <?php
                 } else {
-                ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="signup.php">Sign up</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="signin.php">Sign in</a>
-                </li>
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="signup.php">Sign up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="signin.php">Sign in</a>
+                    </li>
                 <?php } ?>
                 <li class="nav-item">
                     <a class="nav-link" href="#">About</a>
@@ -49,23 +49,25 @@
                     </div>
                 </li>
             </ul>
-            <form class="d-flex">
-                <input class="form-control me-sm-2" type="search" placeholder="Search">
+            <form class="d-flex" action="result.php" method="GET">
+                <input class="form-control me-sm-2" type="search" placeholder="Search" name="search" <?php if (isset($_GET['search'])) {
+                    echo 'value="' . $_GET['search'] . '"';
+                } ?>>
                 <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-               
-            
+
+
             </form>
             <!-- Show username if logged in -->
-            <?php 
-            if (isset($_COOKIE['username'])) {
-            ?>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <span class="nav-link">Welcome, <?php echo $_COOKIE['username']; ?></span>
-                </li>
-            </ul>
             <?php
-            } 
+            if (isset($_COOKIE['username'])) {
+                ?>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <span class="nav-link">Welcome, <?php echo $_COOKIE['username']; ?></span>
+                    </li>
+                </ul>
+                <?php
+            }
             ?>
         </div>
     </div>
